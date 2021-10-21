90e316318ea007925c28a85b6d7b0043c4536e4c
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-10-21 14:39:32 +0200

    created GPSItem class.

---
+++ cohda/GPS.py
@@ -0,0 +1,6 @@
+class GPSItem(object):
+    def __init__(self, ts=0, latitude=0, longitude=0, speed=0):
+        self.ts = ts
+        self.latitude = latitude
+        self.longitude = longitude
+        self.speed = speed
