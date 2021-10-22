9f70bf9d321840d0647aa6da285dbeb43073dbde
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-10-22 09:32:15 +0200

    just for testing

--- Cohdas/BTPheader.py
+++ Cohdas/BTPheader.py
@@ -1,30 +1,30 @@
 import struct
 
-import constant.Constants
+from constant.Constants import *
 
 
 class BTP(object):
     def __init__(self, length, latitude, longitude):
-        self.BTPType = constant.Constants.ETSI_GN_NH_BTPB
-        self.PktTransport = constant.Constants.ETSI_GN_TRANSPORT_GEOBROADCAST
+        self.BTPType = ETSI_GN_NH_BTPB
+        self.PktTransport = ETSI_GN_TRANSPORT_GEOBROADCAST
         self.TrafficClass = 0
         self.MaxPktLifetime = 0
-        self.DestPort = constant.Constants.BTP_PORT_DENM
+        self.DestPort = BTP_PORT_DENM
         self.DestInfo = 0
-        self.CommProfile = constant.Constants.ETSI_GN_PROFILE_ITS_G5
+        self.CommProfile = ETSI_GN_PROFILE_ITS_G5
         self.RepeatInterval = 0
         self.Length = length
-        self.SecProfile = constant.Constants.ETSI_GN_SEC_PROF_AID_SSP
+        self.SecProfile = ETSI_GN_SEC_PROF_AID_SSP
         self.AID = 0x25
         self.SSPLen = 4
         self.SSPBits = [0x01, 0xFF, 0xFF, 0xFF, 0, 0]
         self.Latitude = latitude
         self.Longitude = longitude
-        self.DistanceA = constant.Constants.GN_GBC_DESTDISTANCEA
-        self.DistanceB = constant.Constants.GN_GBC_DESTDISTANCEB
-        self.Shape = constant.Constants.GN_GBC_DESTSHAPE
-        self.Angle = constant.Constants.GN_GBC_DESTANGLE
-        self.Unused: int = 0
+        self.DistanceA = GN_GBC_DESTDISTANCEA
+        self.DistanceB = GN_GBC_DESTDISTANCEB
+        self.Shape = GN_GBC_DESTSHAPE
+        self.Angle = GN_GBC_DESTANGLE
+        self.Unused: int = UNUSED
 
     def assemble_btp_fields(self):
         temp = struct.pack('!BBBBHH',
@@ -36,7 +36,7 @@ class BTP(object):
                            self.DestInfo
                            )
         print("This is the self.Unused that is not an integer: ", self.Unused)
-        temp += struct.pack('!IIHHHBH',
+        temp += struct.pack('!IIHHHBB',
                             self.Latitude,
                             self.Longitude,
                             self.DistanceA,
