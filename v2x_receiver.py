55342ec18031a55b6c274b8a7c54081ebc1e7995
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-10-25 13:55:08 +0200

    debugging the decoding of the message.

--- v2x_receiver.py
+++ v2x_receiver.py
@@ -106,6 +106,8 @@ if __name__ == '__main__':
 
                 if sys.argv[1] == "ITSG5":
                     tmp = data[BTP_HEADER_SIZE:]
+                    print("THis is data[BTP_HEADER_SIZE:] stored in the variable: 'tmp'")
+                    print(tmp)
                     json_data = json.loads(tmp)
                     print("Look here JSON data: ", json_data)
 
